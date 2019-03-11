c50279c4e6aed9060d6fa7fe1ec0b41ab82a0295
Stefan Doychev <sdoychev@gmail.com>
2019-03-11 18:14:32 +0200

    Unity project

---
+++ Task 1/Assets/Invaders/Scripts/EnemyProjectile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    private GameObject target;
+    public GameObject hitFX;
+    public GameObject AsteroidhitFX;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+        if (target == null) return;
+        var direction = target.transform.position - transform.position;
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //targetCollider = GameObject.FindWithTag("Asteroid");
+        if (collision.gameObject.tag.Equals(target.tag))
+        {
+            target.GetComponent<PlayerController>().HitByEnemy();
+            Instantiate(hitFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag.Equals("Asteroid"))
+        {
+            Instantiate(AsteroidhitFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+
+        }
+
+
+    }
+    /*private void OnCollisionEnter2D(Collider2D collision)
+    {
+        //targetCollider = GameObject.FindWithTag("Asteroid");
+        if (collision.gameObject.tag.Equals("Asteroid"))
+        {
+            Instantiate(AsteroidhitFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+
+        }
+        
+    }*/
+}
+
